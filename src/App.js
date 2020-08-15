f6b0b91c8700fcb526432aa3ac1723b639c69412
Marek Surak <marek.surak@firma.seznam.cz>
2020-08-15 09:34:15 +0200

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import config from './config';
+import React, { useEffect, useState } from 'react'
+import config from './config'
 
 const getToken = () => {
   return fetch(`${config.API_URL}/oauth/token`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify ({
+    body: JSON.stringify({
       grant_type: 'client_credentials',
       client_id: config.CLIENT_ID,
-      scope: config.API_SCOPE
-    })
-  }).then(res => res.json())
+      scope: config.API_SCOPE,
+    }),
+  }).then((res) => res.json())
 }
 
-const getSkus = access_token => {
+const getSkus = (access_token) => {
   return fetch(`${config.API_URL}/api/skus`, {
     headers: {
-      'Authorization': `Bearer ${access_token}`
-    }
-  }).then(res => res.json())
+      Authorization: `Bearer ${access_token}`,
+    },
+  }).then((res) => res.json())
 }
 
 function App() {
-  const [ products, setProducts ] = useState([])
-  const [ isLoading, setLoading ] = useState(true)
+  const [products, setProducts] = useState([])
+  const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
     const getProducts = async () => {
@@ -42,12 +42,17 @@ function App() {
   return (
     <>
       <h1> STRV react nights </h1>
-      {isLoading ? '...' : <ul>
-        {products.map(product => <li>{product.attributes.name}</li>)}
-      </ul>}
-      
+      {isLoading ? (
+        '...'
+      ) : (
+        <ul>
+          {products.map((product) => (
+            <li>{product.attributes.name}</li>
+          ))}
+        </ul>
+      )}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-	CLIENT_ID: 'dfxmfT6qfYuYGzf352M_Ikfuq5IBmaQ65dOY-tvjibA',
-	API_URL: 'https://e-shop.commercelayer.io',
-	API_SCOPE: 'market:2942'
+  CLIENT_ID: 'dfxmfT6qfYuYGzf352M_Ikfuq5IBmaQ65dOY-tvjibA',
+  API_URL: 'https://e-shop.commercelayer.io',
+  API_SCOPE: 'market:2942',
 }
