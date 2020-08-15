2f264a886720d0f6bd57224f47240af42c19fdeb
Marek Surak <marek.surak@firma.seznam.cz>
2020-08-15 09:19:52 +0200

    get token, get products

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import config from './config';
+
+const getToken = () => {
+  return fetch(`${config.API_URL}/oauth/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify ({
+      grant_type: 'client_credentials',
+      client_id: config.CLIENT_ID,
+      scope: config.API_SCOPE
+    })
+  }).then(res => res.json())
+}
+
+const getSkus = access_token => {
+  return fetch(`${config.API_URL}/api/skus`, {
+    headers: {
+      'Authorization': `Bearer ${access_token}`
+    }
+  }).then(res => res.json())
+}
 
 function App() {
+  const [ products, setProducts ] = useState([])
+  const [ isLoading, setLoading ] = useState(true)
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const { access_token } = await getToken()
+      const products = await getSkus(access_token)
+
+      setProducts(products.data)
+      setLoading(false)
+    }
+
+    getProducts()
+  })
+
   return (
+    <>
       <h1> STRV react nights </h1>
+      {isLoading ? '...' : <ul>
+        {products.map(product => <li>{product.attributes.name}</li>)}
+      </ul>}
+      
+    </>
   );
 }
 
---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+	CLIENT_ID: 'dfxmfT6qfYuYGzf352M_Ikfuq5IBmaQ65dOY-tvjibA',
+	API_URL: 'https://e-shop.commercelayer.io',
+	API_SCOPE: 'market:2942'
+}
